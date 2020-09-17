e114bb45bfa47187c42e688989388bc627c7da16
Sandipan Ghosh <sandipan.ghosh2005@gmail.com>
2020-09-17 11:42:56 -0400

    Add project files.

---
+++ FileCompare.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FileCompare
+{
+    public class FileCompare
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
